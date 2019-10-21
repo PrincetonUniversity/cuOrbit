0f9c9b2ab329f3650c2cd6e6b09b00388816ba2a
Garrett Wright <gwright@pppl.gov>
2019-10-21 12:47:13 -0400

    fix function signature

--- cuda_helpers.c
+++ cuda_helpers.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
+#include <string.h>
 #include "cuda_helpers.h"
+#include <cuda.h>
+#include <cuda_runtime.h>
 
 /* Make an equiv to calloc for CUDA UMA*/
 void* umacalloc(size_t num, size_t size){
   void* ptr;
 #ifdef __NVCC__
   /* XXXX add cuda check on both of these returns later */
-  cudaMallocManaged(&ptr, num*size);
+  cudaMallocManaged(&ptr, num*size, cudaMemAttachGlobal);
   /* check if host memset faster, our arrays are generally small... */
-  cudaMemset(ptr, 0, num*size);
+  /* cudaMemset(ptr, 0, num*size); */
+  memset(ptr, 0, num*size);
 #else
   ptr = calloc(num, size);
 #endif
