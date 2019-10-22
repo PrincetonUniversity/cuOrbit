c5d5939896f1d2ef37e1f1b6cba013704b42cb7a
Garrett Wright <gwright@pppl.gov>
2019-10-22 14:03:53 -0400

    adds constants header

---
+++ orbit_constants.h
@@ -0,0 +1,13 @@
+#ifndef SET_ORBIT_CONSTANTS_H_
+#define SET_ORBIT_CONSTANTS_H_
+
+#include <math.h>
+
+/* note M_PI was dropped for c99 */
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+const double pi2 = 2. * M_PI;
+const double pi2i = 0.5 / M_PI;
+
+#endif
