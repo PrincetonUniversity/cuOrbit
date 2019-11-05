ce6f96d88c60069aa0cd98dd265c4fd06d6a0816
Garrett Wright <gwright@pppl.gov>
2019-11-05 11:10:25 -0500

    return random

--- orbit_util.c
+++ orbit_util.c
@@ -51,9 +51,7 @@ bool atob(const char* a){
 
 double rand_double(){
   /* stupid simple c rng between 0 1  */
-  //XXXXXXXXXXX return rand()/((double)RAND_MAX + 1);
-  /* for dbg, no random */
-  return 0;
+  return rand()/((double)RAND_MAX + 1);
 }
 
 double rand_double_range(const double low, const double high){
